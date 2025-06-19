5b98f4c3310be81ee9d5fd7ffb7fd54ce7154900
Denys Kotenko <rearden97@gmail.com>
2025-06-19 15:38:46 +0700

    Add `Destination` model

---
+++ HotelsDemo/DestinationPicker/Models/Destination.swift
@@ -0,0 +1,17 @@
+//
+//  Destination.swift
+//  HotelsDemo
+//
+//  Created by Denys Kotenko on 19/6/25.
+//
+
+import Foundation
+
+struct Destination {
+	let id: Int
+	let type: String
+	let name: String
+	let label: String
+	let country: String
+	let cityName: String
+}
