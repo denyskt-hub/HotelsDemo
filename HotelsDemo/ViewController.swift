ae9f7a7987192353975726988f735a8525e3f60c
Denys Kotenko <rearden97@gmail.com>
2025-06-19 12:55:08 +0700

    Initial commit

---
+++ HotelsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotelsDemo
+//
+//  Created by Denys Kotenko on 19/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
