68499de21f6f1c2456e398a6ce0fe16d335af479
Denys Kotenko <rearden97@gmail.com>
2025-06-19 13:27:05 +0700

    Add `SearchCriteria` model

---
+++ HotelsDemo/SearchCriteria/Models/SearchCriteria.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCriteria.swift
+//  HotelsDemo
+//
+//  Created by Denys Kotenko on 19/6/25.
+//
+
+import Foundation
+
+struct SearchCriteria {
+	let destinationId: Int
+	let searchType: String
+	let checkInDate: Date
+	let checkOutDate: Date
+	let adults: Int
+	let childreAge: String
+	let roomsQuantity: Int
+}
