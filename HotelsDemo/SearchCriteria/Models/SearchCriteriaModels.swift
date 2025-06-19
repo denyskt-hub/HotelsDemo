0c6f69b5110ba4c973f5811ead07987bba3ba2e3
Denys Kotenko <rearden97@gmail.com>
2025-06-19 13:56:59 +0700

    Add `SearchCriteriaModels` namespace

---
+++ HotelsDemo/SearchCriteria/Models/SearchCriteriaModels.swift
@@ -0,0 +1,22 @@
+//
+//  SearchCriteriaModels.swift
+//  HotelsDemo
+//
+//  Created by Denys Kotenko on 19/6/25.
+//
+
+import Foundation
+
+enum SearchCriteriaModels {
+	struct Request {}
+
+	struct Response {
+		let criteria: SearchCriteria
+	}
+
+	struct ViewModel {
+		let destination: String
+		let dateRange: String
+		let roomGuests: String
+	}
+}
