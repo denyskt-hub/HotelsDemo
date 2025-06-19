b5db1fdb67ef85535a604fe8613d22fd70afc234
Denys Kotenko <rearden97@gmail.com>
2025-06-19 23:17:48 +0700

    Refactor `SearchCriteriaModels.Load.Response` mapping to `SearchCriteriaModels.Load.ViewModel`

--- HotelsDemo/SearchCriteria/Presenter/SearchCriteriaPresenter.swift
+++ HotelsDemo/SearchCriteria/Presenter/SearchCriteriaPresenter.swift
@@ -15,11 +15,19 @@ final class SearchCriteriaPresenter: SearchCriteriaPresentationLogic {
 	weak var viewController: SearchCriteriaDisplayLogic?
 
 	func presentCriteria(response: SearchCriteriaModels.Load.Response) {
+		let dareRange = "\(response.criteria.checkInDate) - \(response.criteria.checkOutDate)"
+
+		let adults = "\(response.criteria.adults) adult(s)"
+		let children = "\(response.criteria.childrenAge?.components(separatedBy: ",").count ?? 0) child(ren)"
+		let rooms = "\(response.criteria.roomsQuantity) room(s)"
+		let roomGuests = "\(rooms) for \(adults), \(children)"
+
 		let viewModel = SearchCriteriaModels.Load.ViewModel(
 			destination: response.criteria.destination?.label,
-			dateRange: "\(response.criteria.checkInDate) - \(response.criteria.checkOutDate)",
-			roomGuests: "\(response.criteria.roomsQuantity) room(s) for \(response.criteria.adults) adult(s)"
+			dateRange: dareRange,
+			roomGuests: roomGuests
 		)
+
 		viewController?.displayCriteria(viewModel: viewModel)
 	}
 }
