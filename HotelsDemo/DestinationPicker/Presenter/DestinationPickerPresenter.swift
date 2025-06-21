e277a74c6869b77da9a7198f3138915c54c34b53
Denys Kotenko <rearden97@gmail.com>
2025-06-21 20:10:37 +0700

    Move `DestinationPickerPresentationLogic` to its own file

--- HotelsDemo/DestinationPicker/Presenter/DestinationPickerPresenter.swift
+++ HotelsDemo/DestinationPicker/Presenter/DestinationPickerPresenter.swift
@@ -7,12 +7,6 @@
 
 import Foundation
 
-protocol DestinationPickerPresentationLogic {
-	func presentDestinations(response: DestinationPickerModels.Search.Response)
-	func presentSelectedDestination(response: DestinationPickerModels.Select.Response)
-	func presentSearchError(_ error: Error)
-}
-
 final class DestinationPickerPresenter: DestinationPickerPresentationLogic {
 	weak var viewController: DestinationPickerDisplayLogic?
 
