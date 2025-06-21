8dbe502b90d77d1a05e749f5c2bba02450bc4e9b
Denys Kotenko <rearden97@gmail.com>
2025-06-21 20:02:06 +0700

    Move `RoomGuestsPickerPresentationLogic` protocol to its own file

--- HotelsDemo/RoomGuestsPicker/Presenter/RoomGuestsPickerPresenter.swift
+++ HotelsDemo/RoomGuestsPicker/Presenter/RoomGuestsPickerPresenter.swift
@@ -7,19 +7,7 @@
 
 import Foundation
 
-protocol RoomGuestsPickerPresentationLogic {
-	func presentLimits(response: RoomGuestsPickerModels.LoadLimits.Response)
-	func presentRoomGuests(response: RoomGuestsPickerModels.Load.Response)
 
-	func presentUpdateRooms(response: RoomGuestsPickerModels.UpdateRooms.Response)
-	func presentUpdateAdults(response: RoomGuestsPickerModels.UpdateAdults.Response)
-	func presentUpdateChildrenAge(response: RoomGuestsPickerModels.UpdateChildrenAge.Response)
-
-	func presentAgePicker(response: RoomGuestsPickerModels.AgeSelection.Response)
-	func presentChildrenAge(response: RoomGuestsPickerModels.AgeSelected.Response)
-
-	func presentSelectedRoomGuests(response: RoomGuestsPickerModels.Select.Response)
-}
 
 final class RoomGuestsPickerPresenter: RoomGuestsPickerPresentationLogic {
 	weak var viewController: RoomGuestsPickerDisplayLogic?
