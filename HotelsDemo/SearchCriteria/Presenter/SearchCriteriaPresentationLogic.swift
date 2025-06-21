8c39b35cfc95554dd39bad738f9eaadde29c0134
Denys Kotenko <rearden97@gmail.com>
2025-06-21 20:13:58 +0700

    Move `SearchCriteriaPresentationLogic` protocol to its own file

---
+++ HotelsDemo/SearchCriteria/Presenter/SearchCriteriaPresentationLogic.swift
@@ -0,0 +1,19 @@
+//
+//  SearchCriteriaPresentationLogic.swift
+//  HotelsDemo
+//
+//  Created by Denys Kotenko on 21/6/25.
+//
+
+import Foundation
+
+protocol SearchCriteriaPresentationLogic {
+	func presentCriteria(response: SearchCriteriaModels.Load.Response)
+	func presentLoadError(_ error: Error)
+
+	func presentRoomGuests(response: SearchCriteriaModels.LoadRoomGuests.Response)
+
+	func presentCriteria(response: SearchCriteriaModels.UpdateDestination.Response)
+	func presentCriteria(response: SearchCriteriaModels.UpdateRoomGuests.Response)
+	func presentUpdateError(_ error: Error)
+}
--- HotelsDemo/SearchCriteria/Presenter/SearchCriteriaPresenter.swift
+++ HotelsDemo/SearchCriteria/Presenter/SearchCriteriaPresenter.swift
@@ -7,17 +7,6 @@
 
 import Foundation
 
-protocol SearchCriteriaPresentationLogic {
-	func presentCriteria(response: SearchCriteriaModels.Load.Response)
-	func presentLoadError(_ error: Error)
-
-	func presentRoomGuests(response: SearchCriteriaModels.LoadRoomGuests.Response)
-
-	func presentCriteria(response: SearchCriteriaModels.UpdateDestination.Response)
-	func presentCriteria(response: SearchCriteriaModels.UpdateRoomGuests.Response)
-	func presentUpdateError(_ error: Error)
-}
-
 final class SearchCriteriaPresenter: SearchCriteriaPresentationLogic {
 	weak var viewController: SearchCriteriaDisplayLogic?
 
