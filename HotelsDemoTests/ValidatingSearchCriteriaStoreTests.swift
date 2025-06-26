d675bdf51c071e9ee57475f00fc3564d99113a77
Denys Kotenko <rearden97@gmail.com>
2025-06-26 16:26:43 +0700

    `ValidatingSearchCriteriaStore.retrieve` delivers validated search criteria and saves it when stored criteria is invalid

--- HotelsDemoTests/ValidatingSearchCriteriaStoreTests.swift
+++ HotelsDemoTests/ValidatingSearchCriteriaStoreTests.swift
@@ -87,6 +87,31 @@ final class ValidatingSearchCriteriaStoreTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 	}
 
+	func test_retrieve_deliversValidatedSearchCriteriaAndSavesItWhenStoredCriteriaIsInvalid() {
+		let invalid = anySearchCriteria()
+		let valid = makeSearchCriteria(checkInDate: "27.06.2025".date(), checkOutDate: "28.06.2025".date())
+		let (sut, store, validator) = makeSUT()
+		validator.stubbedResult = valid
+
+		let exp = expectation(description: "Wait for completion")
+
+		sut.retrieve { result in
+			switch result {
+			case let .success(retrieved):
+				XCTAssertEqual(retrieved, valid)
+				XCTAssertEqual(store.messages, [.retrieve, .save(valid)])
+
+			case let .failure(error):
+				XCTFail("Expected success, got \(error) instead")
+			}
+			exp.fulfill()
+		}
+
+		store.completeRetrieve(with: .success(invalid))
+
+		wait(for: [exp], timeout: 1.0)
+	}
+
 	// MARK: - Helpers
 
 	private func makeSUT() -> (sut: ValidatingSearchCriteriaStore, store: SearchCriteriaStoreSpy, validator: SearchCriteriaValidatorSpy) {
