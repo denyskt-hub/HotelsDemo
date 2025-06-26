feec632ac5692bb62c7615bb220af44a22dab8c8
Denys Kotenko <rearden97@gmail.com>
2025-06-26 12:13:01 +0700

    `DefaultSearchCriteriaValidator.validate` returns fixed criteria when check in date in past

--- HotelsDemoTests/DefaultSearchCriteriaValidatorTests.swift
+++ HotelsDemoTests/DefaultSearchCriteriaValidatorTests.swift
@@ -12,15 +12,33 @@ final class DefaultSearchCriteriaValidatorTests: XCTestCase {
 	func test_validate_returnsSameCriteria_whenAllIsValid() {
 		let currentDate = "26.06.2025".date()
 		let calendar = Calendar.gregorian()
-		let sut = makeSUT(calendar: calendar, currentDate: { currentDate })
-
-		let validCriteria = makeValidCriteria(
+		let validCriteria = makeValidSearchCriteria(
 			calendar: calendar,
 			currentDate: currentDate
 		)
+		let sut = makeSUT(calendar: calendar, currentDate: { currentDate })
+
 		let result = sut.validate(validCriteria)
 
 		XCTAssertEqual(result, validCriteria)
+	}
+
+	func test_validate_returnsFixedCriteria_whenCheckInDateIsInPast() {
+		let currentDate = "26.06.2025".date()
+		let calendar = Calendar.gregorian()
+		let invalidCriteria = makeSearchCriteria(
+			checkInDate: currentDate.adding(seconds: -1),
+			checkOutDate: currentDate.adding(days: 1, calendar: calendar)
+		)
+		let expectedCriteria = makeSearchCriteria(
+			checkInDate: currentDate.adding(days: 1, calendar: calendar),
+			checkOutDate: currentDate.adding(days: 2, calendar: calendar)
+		)
+		let sut = makeSUT(calendar: calendar, currentDate: { currentDate })
+
+		let result = sut.validate(invalidCriteria)
+
+		XCTAssertEqual(result, expectedCriteria)
 	}
 
 	// MARK: - Helpers
@@ -35,11 +53,24 @@ final class DefaultSearchCriteriaValidatorTests: XCTestCase {
 		)
 	}
 
-	private func makeValidCriteria(calendar: Calendar, currentDate: Date) -> SearchCriteria {
+	private func makeValidSearchCriteria(
+		calendar: Calendar,
+		currentDate: Date
+	) -> SearchCriteria {
+		makeSearchCriteria(
+			checkInDate: currentDate,
+			checkOutDate: currentDate.adding(days: 1, calendar: calendar)
+		)
+	}
+
+	private func makeSearchCriteria(
+		checkInDate: Date,
+		checkOutDate: Date
+	) -> SearchCriteria {
 		SearchCriteria(
 			destination: nil,
-			checkInDate: currentDate,
-			checkOutDate: currentDate.adding(days: 1, calendar: calendar),
+			checkInDate: checkInDate,
+			checkOutDate: checkOutDate,
 			adults: 2,
 			childrenAge: [],
 			roomsQuantity: 1
