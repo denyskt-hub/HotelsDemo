6f4119512b1cb2cff97b55f145c38e1407397dfb
Denys Kotenko <rearden97@gmail.com>
2025-07-20 11:54:19 +0700

    Add generic `Specification` protocol with `AndSpecification` and `OrSpecification`

---
+++ HotelsDemo/Shared/Specification/Specification.swift
@@ -0,0 +1,24 @@
+//
+//  Specification.swift
+//  HotelsDemo
+//
+//  Created by Denys Kotenko on 20/7/25.
+//
+
+import Foundation
+
+public protocol Specification {
+	associatedtype Item
+
+	func isSatisfied(by item: Item) -> Bool
+}
+
+public extension Specification {
+	func and(_ other: Self) -> AndSpecification<Self> {
+		AndSpecification(lhs: self, rhs: other)
+	}
+
+	func or(_ other: Self) -> OrSpecification<Self> {
+		OrSpecification(lhs: self, rhs: other)
+	}
+}
