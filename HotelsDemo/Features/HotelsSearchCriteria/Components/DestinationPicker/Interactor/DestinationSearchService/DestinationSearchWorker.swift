f9a98ed665fc9d77b2d229beff09e814276bca58
Denys Kotenko <rearden97@gmail.com>
2025-11-05 23:12:05 +0700

    Remove query validation from `DestinationSearchWorker` (handled by `DestinationPickerInteractor`)

--- HotelsDemo/Features/HotelsSearchCriteria/Components/DestinationPicker/Interactor/DestinationSearchService/DestinationSearchWorker.swift
+++ HotelsDemo/Features/HotelsSearchCriteria/Components/DestinationPicker/Interactor/DestinationSearchService/DestinationSearchWorker.swift
@@ -11,10 +11,6 @@ public final class DestinationSearchWorker: DestinationSearchService {
 	private let factory: DestinationsRequestFactory
 	private let client: HTTPClient
 
-	private enum Error: Swift.Error {
-		case invalidQuery
-	}
-
 	public init(
 		factory: DestinationsRequestFactory,
 		client: HTTPClient
@@ -24,15 +20,6 @@ public final class DestinationSearchWorker: DestinationSearchService {
 	}
 
 	public func search(query: String, completion: @escaping (DestinationSearchService.Result) -> Void) {
-		#if DEBUG
-		assert(!query.trimmingCharacters(in: .whitespaces).isEmpty, "Query must not be empty")
-		#endif
-
-		guard !query.trimmingCharacters(in: .whitespaces).isEmpty else {
-			completion(.failure(Error.invalidQuery))
-			return
-		}
-
 		let request = factory.makeSearchRequest(query: query)
 
 		client.perform(request) { result in
