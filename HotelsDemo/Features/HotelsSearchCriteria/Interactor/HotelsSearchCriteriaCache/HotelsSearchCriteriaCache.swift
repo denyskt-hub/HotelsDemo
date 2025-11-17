817470609bbb1884e5338729c49a1550a2e86b43
Denys Kotenko <rearden97@gmail.com>
2025-11-17 21:45:44 +0700

    Add async `save` to `HotelsSearchCriteriaCache` with a default bridging implementation

--- HotelsDemo/Features/HotelsSearchCriteria/Interactor/HotelsSearchCriteriaCache/HotelsSearchCriteriaCache.swift
+++ HotelsDemo/Features/HotelsSearchCriteria/Interactor/HotelsSearchCriteriaCache/HotelsSearchCriteriaCache.swift
@@ -10,5 +10,23 @@ import Foundation
 public protocol HotelsSearchCriteriaCache: Sendable {
 	typealias SaveResult = Result<Void, Error>
 
+	@available(*, deprecated, message: "Use async function instead")
 	func save(_ criteria: HotelsSearchCriteria, completion: @Sendable @escaping (SaveResult) -> Void)
+
+	func save(_ criteria: HotelsSearchCriteria) async throws
+}
+
+extension HotelsSearchCriteriaCache {
+	public func save(_ criteria: HotelsSearchCriteria) async throws {
+		try await withCheckedThrowingContinuation { continuation in
+			save(criteria) { result in
+				switch result {
+				case .success:
+					continuation.resume(returning: ())
+				case let .failure(error):
+					continuation.resume(throwing: error)
+				}
+			}
+		}
+	}
 }
