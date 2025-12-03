25af63806f51251c358e4e94a5e79377ea8d762d
Denys Kotenko <rearden97@gmail.com>
2025-12-03 20:16:52 +0700

    Add async `save` and `data` method to `ImageDataCache` with bridge implementation to callback-based API

--- HotelsDemo/Shared/ImageLoading/ImageDataCache/ImageDataCache.swift
+++ HotelsDemo/Shared/ImageLoading/ImageDataCache/ImageDataCache.swift
@@ -13,11 +13,44 @@ public protocol ImageDataCache: Sendable {
 
 	/// The completion handler can be invoked in any thread.
 	/// Clients are responsible to dispatch to appropriate threads, if needed.
+	@available(*, deprecated, message: "Use async version")
 	func save(_ data: Data, forKey key: String, completion: @Sendable @escaping (SaveResult) -> Void)
 
 	/// The completion handler can be invoked in any thread.
 	/// Clients are responsible to dispatch to appropriate threads, if needed.
+	@available(*, deprecated, message: "Use async version")
 	func data(forKey key: String, completion: @Sendable @escaping (DataResult) -> Void)
+
+	func save(_ data: Data, forKey key: String) async throws
+	func data(forKey key: String) async throws -> Data?
+}
+
+extension ImageDataCache {
+	public func save(_ data: Data, forKey key: String) async throws {
+		try await withCheckedThrowingContinuation { continuation in
+			save(data, forKey: key) { result in
+				switch result {
+				case .success:
+					continuation.resume(returning: ())
+				case let .failure(error):
+					continuation.resume(throwing: error)
+				}
+			}
+		}
+	}
+
+	public func data(forKey key: String) async throws -> Data? {
+		try await withCheckedThrowingContinuation { continuation in
+			data(forKey: key) { result in
+				switch result {
+				case let .success(data):
+					continuation.resume(returning: data)
+				case let .failure(error):
+					continuation.resume(throwing: error)
+				}
+			}
+		}
+	}
 }
 
 extension ImageDataCache {
