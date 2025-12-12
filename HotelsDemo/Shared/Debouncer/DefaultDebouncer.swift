e9688f6b0dba51e7a153e3f430ffa80668c14174
Denys Kotenko <denyskt.hub@gmail.com>
2025-12-12 14:10:07 +0200

    Replace `Mutex` in `DefaultDebouncer` with `TaskStore` for task management

--- HotelsDemo/Shared/Debouncer/DefaultDebouncer.swift
+++ HotelsDemo/Shared/Debouncer/DefaultDebouncer.swift
@@ -6,36 +6,36 @@
 //
 
 import Foundation
-import Synchronization
 
 public final class DefaultDebouncer: Debouncer {
 	private let delay: TimeInterval
-	private let currentSyncTask = Mutex<Task<Void, Never>?>(nil)
-	private let currentAsyncTask = Mutex<Task<Void, Never>?>(nil)
+
+	private let syncTaskStore = TaskStore<Void, Never>()
+	private let asyncTaskStore = TaskStore<Void, Never>()
 
 	public init(delay: TimeInterval) {
 		self.delay = delay
 	}
 
 	public func execute(_ action: @Sendable @escaping () -> Void) {
-		currentSyncTask.withLock { task in
-			task?.cancel()
-			task = Task { @MainActor in
-				try? await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
-				guard !Task.isCancelled else { return }
-				action()
-			}
+		let task = Task { @MainActor in
+			try? await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
+			guard !Task.isCancelled else { return }
+			action()
+		}
+		Task {
+			await syncTaskStore.setTask(task)
 		}
 	}
 
 	public func asyncExecute(_ action: @Sendable @escaping () async -> Void) {
-		currentAsyncTask.withLock { task in
-			task?.cancel()
-			task = Task {
-				try? await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
-				guard !Task.isCancelled else { return }
-				await action()
-			}
+		let task = Task {
+			try? await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
+			guard !Task.isCancelled else { return }
+			await action()
+		}
+		Task {
+			await asyncTaskStore.setTask(task)
 		}
 	}
 }
